37e66698adda08b8ca69a969d8fb09ee3ea3094f
yerkebulan0 <erke96965@gmail.com>
2025-10-18 16:15:36 +0500

    fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 // Simple endpoint qwlekqe
 app.get("/api/hello", (req, res) => {
   // some logic...
-  res.json({ message: "Hello from Ubuntu server!", some: "sdsdada" });
+  res.json({ message: "Hello from sdsqd server!", some: "sdsdada" });
 });
 
 // For testing purposes
